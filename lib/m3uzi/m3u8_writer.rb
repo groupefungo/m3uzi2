4e727c1f11531b14a77864d978fde8a411352057
os6sense <leej@sowhatresearch.com>
2014-09-02 22:14:04 +0100

    File check moved to write_file, fixed \r\n, tweaked write_header to write_headers

--- lib/m3uzi/m3u8_writer.rb
+++ lib/m3uzi/m3u8_writer.rb
@@ -1,7 +1,7 @@
-require_relative "error_handler"
+require_relative 'error_handler'
 
 module M3Uzi2
-  # blah
+  # Write an m3u8 file
   class M3U8Writer
     include ErrorHandler
     def initialize(m3u8_file)
@@ -20,11 +20,11 @@ module M3Uzi2
     end
 
     def write(stream = nil)
-      handle_error('No M3U8File specified', true) if @m3u8_file.nil?
       stream.nil? ? write_file(@m3u8_file.pathname) : write_io_stream(stream)
     end
 
     def write_file(pathname)
+      handle_error('No M3U8File specified', true) if @m3u8_file.nil?
       File.open(pathname, 'w') do | f |
         f.flock(File::LOCK_EX) if @write_method == :flock
         write_io_stream(f)
@@ -32,21 +32,21 @@ module M3Uzi2
     end
 
     def write_io_stream(stream)
-      write_header(stream, @m3u8_file.headers)
-      write_playlist(stream, @m3u8_file.headers)
+      write_headers(stream, @m3u8_file.headers)
+      write_playlist(stream, @m3u8_file.playlist)
     end
 
     private
 
     def write_playlist(io_stream, playlist)
       playlist.each do | item |
-        io_stream << item << '\n'
+        io_stream << item << "\r\n"
       end
     end
 
-    def write_header(io_stream, headers)
+    def write_headers(io_stream, headers)
       headers.each do | item |
-        io_stream << item << '\n'
+        io_stream << item << "\r\n"
       end
     end
   end

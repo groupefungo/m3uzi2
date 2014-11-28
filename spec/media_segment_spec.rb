45b2cd5d6ebac5b95bc88fd0424bd087545e9625
os6sense <leej@sowhatresearch.com>
2014-11-28 23:02:29 +0000

    Outline spec for media segment

--- spec/media_segment_spec.rb
+++ spec/media_segment_spec.rb
@@ -1,2 +1,49 @@
 require_relative 'spec_helper'
+require_relative '../lib/m3uzi/specification/m3u8_specification'
+require_relative '../lib/m3uzi/types/media_segment'
+
+describe M3Uzi2::MediaSegment do
+
+  describe :path do
+  end
+
+  describe :initialize do
+  end
+
+  describe :value do
+  end
+
+  describe :version do
+  end
+
+  describe :playlist_compatability do
+  end
+
+  describe :to_s do
+  end
+
+  describe :valid? do
+  end
+
+  describe :add_segment_tag do
+  end
+
+  describe :clear_segment_tag do
+  end
+
+  describe :remove_segment_tag do
+  end
+
+  describe :has_tag? do
+  end
+
+
+  describe :applicable_tags do
+  end
+
+  describe :duration do
+  end
+
+
+end
 

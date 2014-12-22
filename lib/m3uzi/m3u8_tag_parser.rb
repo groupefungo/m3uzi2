3f7e22ef7843f64a23fc046bd15cef2df597e26a
os6sense <leej@sowhatresearch.com>
2014-12-22 09:20:49 +0000

    Back to basics - refactor of the tag parser, adding tests

--- lib/m3uzi/m3u8_tag_parser.rb
+++ lib/m3uzi/m3u8_tag_parser.rb
@@ -1,17 +1,9 @@
 module M3Uzi2
-  # Abstraction for a line of a file that has been parsed into tag,
-  # attribute, value.
-  class M3U8ParsedTag
-    attr_reader :tag,
-                :attributes,
-                :value
-    def initialize(tag = nil, attributes = nil, value = nil)
-      @tag, @attributes, @value = tag, attributes, value
-    end
-  end
-
   # Tags parsed as of version 7 (RFC revision 13)
-  # EXT-X-SESSION-DATA
+  #
+  # As versions 6 plus:
+  #
+  # EXT-X-SESSION-DATA:<attribute list>
 
   # Tags parsed as of version 6 (RFC revision 13)
   # http://tools.ietf.org/html/draft-pantos-http-live-streaming-13
@@ -40,54 +32,82 @@ module M3Uzi2
 
   # Note that the parser is primarily used by the reader
   class M3U8TagParser
+    # Abstraction for a line of a file that has been parsed into tag,
+    # attribute, value.
+    class ParsedTag
+      attr_reader :tag,
+                  :attributes,
+                  :value
+
+      def initialize(tag = nil, attributes = nil, value = nil)
+        @tag, @attributes, @value = tag, attributes, value
+      end
+    end
+
+    INDEPENDENT_TAGS = %w( #EXTM3U
+                           #EXT-X-ENDLIST
+                           #EXT-X-DISCONTINUITY
+                           #EXT-X-I-FRAMES-ONLY
+                           #EXT-X-INDEPENDENT-SEGMENTS )
+
+    VALUE_TAGS       = %w( #EXTINF
+                           #EXT-X-BYTERANGE
+                           #EXT-X-TARGETDURATION
+                           #EXT-X-MEDIA-SEQUENCE
+                           #EXT-X-PROGRAM-DATE-TIME
+                           #EXT-X-ALLOW-CACHE
+                           #EXT-X-PLAYLIST-TYPE
+                           #EXT-X-DISCONTINUITY-SEQUENCE
+                           #EXT-X-VERSION )
+
+    ATTRIBUTE_TAGS   = %w( #EXT-X-KEY
+                           #EXT-X-MEDIA
+                           #EXT-X-STREAM-INF
+                           #EXT-X-MAP
+                           #EXT-X-I-FRAME-STREAM-INF
+                           #EXT-X-START
+                           #EXT-X-SESSION-DATA )
+
     def parse(line, _m3u8_file)
-      parse_line(line)
+      return nil unless valid?(line)
+      return nil if (value = parse_line(line)).nil?
+      ParsedTag.new(*value)
     end
 
     # on return only attributes OR value should be set, never both
     def parse_line(line)
-      return nil unless valid?(line)
-
       # it isn't a tag if it doesnt begin with #
-      return M3U8ParsedTag.new(nil, nil, line) unless line[0] == '#'
+      return [nil, nil, line] unless line[0] == '#'
 
       # is it an idependent tag?
-      return M3U8ParsedTag.new(line[1..-1]) if independent_tag?(line)
+      return [line[1..-1], nil, nil] if independent_tag?(line)
       return nil unless (pos = line.index(':'))
 
       # is it a VALUE tag?
       tag, attr_or_val = [line[1..pos - 1], line[pos + 1..-1]]
-      return M3U8ParsedTag.new(tag, nil, attr_or_val) if value_tag?(line[0..pos - 1])
-      return M3U8ParsedTag.new(tag, attr_or_val) if attribute_tag?(line[0..pos - 1])
+      return [tag, nil, attr_or_val] if value_tag?(line[0..pos - 1])
+
+      # it *should* be an attribute tag.
+      return [tag, attr_or_val, nil] if attribute_tag?(line[0..pos - 1])
 
       # I don't know wtf it is!
       nil
     end
 
-    private
-
     def independent_tag?(tag)
-      tags = %w( #EXTM3U #EXT-X-ENDLIST #EXT-X-DISCONTINUITY
-                 #EXT-X-I-FRAMES-ONLY #EXT-X-INDEPENDENT-SEGMENTS )
-      return tags.include?(tag)
+      return INDEPENDENT_TAGS.include?(tag)
     end
 
     def value_tag?(tag)
-      tags = %w( #EXTINF #EXT-X-BYTERANGE #EXT-X-TARGETDURATION
-                 #EXT-X-MEDIA-SEQUENCE #EXT-X-PROGRAM-DATE-TIME
-                 #EXT-X-ALLOW-CACHE #EXT-X-PLAYLIST-TYPE
-                 #EXT-X-DISCONTINUITY-SEQUENCE #EXT-X-VERSION )
-      return tags.include?(tag)
+      return VALUE_TAGS.include?(tag)
     end
 
     def attribute_tag?(tag)
-      tags = %w( #EXT-X-KEY #EXT-X-MEDIA #EXT-X-STREAM-INF #EXT-X-MAP
-                 #EXT-X-I-FRAME-STREAM-INF #EXT-X-START #EXT-X-SESSION-DATA )
-      return tags.include?(tag)
+      return ATTRIBUTE_TAGS.include?(tag)
     end
 
+    # TODO
     # Test if a line is valid which means no whitespace seperating tags
-    #
     def valid?(line)
       true
     end
--- spec/m3u8_tag_parser_spec.rb
+++ spec/m3u8_tag_parser_spec.rb
@@ -1,2 +1,26 @@
 require_relative 'spec_helper'
+require_relative '../lib/m3uzi/m3u8_tag_parser'
+
+describe :M3U8ParsedTag do
+
+end
+
+describe :M3U8TagParser do
+
+  it 'returns a tag with only the value set unless the line starts with #' do
+  end
+
+
+
+  describe :valid? do
+    it 'returns a tag if the line is valid' do
+    end
+
+    it 'returns false if whitespace sperate tags' do
+    end
+  end
+
+
+
+end
 
